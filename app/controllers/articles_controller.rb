e1e6ceb1cf6e06fb50ad1231d9784e8e62d93537
Leonardo Nascimento <leonardo.nascimento@armateus.com.br>
2019-01-15 13:50:09 -0300

    feat(article): add edit function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,10 +9,27 @@ class ArticlesController < ApplicationController
 
   def new; end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
-    article = Article.new(article_params)
-    article.save
-    redirect_to article
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
 
   private
